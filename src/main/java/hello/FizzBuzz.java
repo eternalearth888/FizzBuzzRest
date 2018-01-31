b82800218d4507a8bd997b62569b16cc3a445191
Maria Deslis <mdeslis@gmail.com>
2018-01-31 12:58:55 -0700

    Initial commit

---
+++ src/main/java/hello/FizzBuzz.java
@@ -0,0 +1,47 @@
+package hello;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+    private final String upperBound;
+    private final List<Integer> Fizz = new ArrayList<>();
+    private final List<Integer> Buzz = new ArrayList<>();
+    private final List<Integer> FizzBuzz = new ArrayList<>();
+
+    public FizzBuzz(String upperBound) {
+        this.upperBound = upperBound;
+    }
+
+    public String getUpperBound(){
+        try{
+            Integer.valueOf(upperBound);
+            calculateFizzBuzz();
+        }
+        catch (NumberFormatException e){
+            return upperBound + " : NOT VALID INTEGER";
+        }
+        return upperBound;
+    }
+
+    public List<Integer> getBuzz() {
+        return Buzz;
+    }
+
+    public List<Integer> getFizzBuzz() {
+        return FizzBuzz;
+    }
+
+    public List<Integer> getFizz() {
+        return Fizz;
+    }
+
+    public void calculateFizzBuzz(){
+        // grab whole number range of 1 - content (content is our upperbound)
+        IntStream.rangeClosed(1, Integer.parseInt(upperBound))
+                .mapToObj(i -> i % 3 == 0 ?
+                            (i % 5 == 0 ? FizzBuzz.add(i) : Fizz.add(i)) :
+                            (i%5==0 ? Buzz.add(i) : i))
+                .forEach(i -> System.out.print(i));
+    }
+}
---
+++ src/main/java/hello/FizzBuzzController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+public class FizzBuzzController {
+    @RequestMapping("/fizzbuzz")
+    public FizzBuzz fizzBuzz(
+            // @RequestParm binds the value of the query string parameter upperBound into the upperBound parameter in FizzBuzz
+            @RequestParam(value="upperBound", defaultValue="No parameter value given") String upperBound) {
+                    return new FizzBuzz(upperBound);
+    }
+}
